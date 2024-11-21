2047171efcc9cdf7c4adc9d5cc84e3bc33a680b2
udaya-krishnan <udayankrishnan36@gmail.com>
2024-11-21 12:42:33 +0530

    bug fix

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const userRuter=require('./router/UserRuter')
 require('dotenv').config()
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://stock-image-platform-nine.vercel.app"], // Add all allowed origins
+  origin: ["https://stock-image-platform-nine.vercel.app"], // Add all allowed origins
   credentials: true, // Enable sending cookies or authorization headers
   methods: ["GET", "POST", "PUT", "DELETE"], // Define allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
